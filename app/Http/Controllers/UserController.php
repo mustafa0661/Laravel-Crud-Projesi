345edb44cd8bf1cf5f810df7b43c1e1e39b2e682
Mustafa Uzun <mustafa.uzun1961ts@gmail.com>
2024-09-13 12:16:40 +0300

    home.blade login ve logout eklendi. User.Controller'a login ve logout fonksiyonları eklendi. web.php routelar eklendi.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,20 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+           'loginname' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if(auth()->attempt(['name' => $incomingFields['loginname'],
+            'password' => $incomingFields['loginpassword']])){
+            $request->session()->regenerate();
+        }
+
+        return redirect('/');
+    }
+    public function logout(){
+        auth()->logout();
+        return redirect('/');
+    }
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'name' => ['required', 'min:3', 'max:20'],
-            'email' => ['required', 'email', 'min:3', 'max:255'],
+            'name' => ['required', 'min:3', 'max:20', Rule::unique('users', 'name')],
+            'email' => ['required', 'email', 'min:3', 'max:255', Rule::unique('users', 'email')],
             'password' => ['required', 'min:3', 'max:255']
         ]);
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
-        User::create($incomingFields);
-
-        return 'Hello from our controller';
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/');
     }
 }
