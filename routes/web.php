cce54484c9819016d9b842dd4d4d546907e2eb1b
Mustafa Uzun <mustafa.uzun1961ts@gmail.com>
2024-09-18 20:28:06 +0300

    Kullanıcıya göre postlar listele işlemi tanımlandı

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    $posts = [];
+    if(auth()->check()){
+        $posts = auth()->user()->usersCoolPosts()->latest()->get();
+    }
+    //$posts = Post::where('user_id', auth()->id())->get();
+    return view('home', ['posts' => $posts]);
 });
 
 Route::post('/register', [UserController::class, 'register']);
