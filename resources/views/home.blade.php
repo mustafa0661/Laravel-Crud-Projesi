5e7f5accfa60d9a42013547c59a00012302cfcbe
Mustafa Uzun <mustafa.uzun1961ts@gmail.com>
2024-09-18 20:10:17 +0300

    Post ekleme kısmı eklendi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,16 @@
             @csrf
             <button>Log Out</button>
         </form>
+
+        <div style="border: 3px solid black;">
+            <h2>Create a New Post</h2>
+            <form action="/create-post" method="POST">
+                @csrf
+                <input type="text" name="title" placeholder="title">
+                <textarea name="body" placeholder="body content..."></textarea>
+                <button>Save Post</button>
+            </form>
+        </div>
     @else
         <div style="border: 3px solid black;">
             <h2>Register</h2>
